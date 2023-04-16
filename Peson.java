642ca08923d9fe6f53aa870d669497ebebc0f1e5
DjUnited27 <83501663+DjUnited27@users.noreply.github.com>
2023-04-16 18:45:17 +0400

    Update Peson.java
    Signed-off-by: DjUnited27 <83501663+DjUnited27@users.noreply.github.com>

--- Peson.java
+++ Peson.java
@@ -3,18 +3,65 @@ package com.example.chatgptprojectappv11;
 public class Person {
     private String name;
     private String surname;
-    private String company;
     private String birthday;
+    private String birthMonth;
+    private String birthYear;
+    private String company;
 
-    public Person(String name, String surname, String company, String birthday) {
+    public Person(String name, String surname, String birthday, String birthMonth, String birthYear, String company) {
         this.name = name;
         this.surname = surname;
+        this.birthday = birthday;
+        this.birthMonth = birthMonth;
+        this.birthYear = birthYear;
         this.company = company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
         this.birthday = birthday;
     }
 
-    @Override
-    public String toString() {
-        return name + "," + surname + "," + company + "," + birthday;
+    public String getBirthMonth() {
+        return birthMonth;
+    }
+
+    public void setBirthMonth(String birthMonth) {
+        this.birthMonth = birthMonth;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(String birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
     }
 }
